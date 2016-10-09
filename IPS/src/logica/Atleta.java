cefe7f06718687929de2774fb96f29d5da9dba18
Javier C. f(z) <javitxu1995@gmail.com>
2016-10-09 20:04:02 +0200

    Alguna clase algo mas preparada

--- IPS/src/logica/Atleta.java
+++ IPS/src/logica/Atleta.java
@@ -1,5 +1,40 @@
 package logica;
 
-public class Atleta {
+import java.sql.Date;
 
+public class Atleta {
+	String DNI;
+	String nombre;
+	String categoria;
+	Date fechaInscripcion;
+	public Atleta(String dni, String nombre, String categoria, Date fecha){
+		this.DNI = dni;
+		this.nombre = nombre;
+		this.categoria = categoria;
+		this.fechaInscripcion = fecha;
+	}
+	private void setDNI(String dNI) {
+		DNI = dNI;
+	}
+	private void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	private void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	private void setFechaInscripcion(Date fechaInscripcion) {
+		this.fechaInscripcion = fechaInscripcion;
+	}
+	public String getDNI() {
+		return DNI;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public Date getFechaInscripcion() {
+		return fechaInscripcion;
+	}
 }
--- IPS/src/logica/Evento.java
+++ IPS/src/logica/Evento.java
@@ -1,13 +1,74 @@
 package logica;
 
+import java.sql.Date;
 import java.util.ArrayList;
 
 public class Evento {
 
-	private ArrayList<Atleta> atletas = new ArrayList<Atleta>();
-	private ArrayList<Inscrito> carrera;
+	private ArrayList<Atleta> atletas;
+	private ArrayList<Inscrito> participantes;
+
+	// Datos del evento
+	String nombre;
+	String tipo;
+	int precio;
+	Date fechaCompeticion;
+	Date fechaFinInscripcion;
+	//De momento se considera una unica fecha de cada tipo. 
 	
-	public String toString(){
+	public Evento(String name, String type, int price, Date dateComp, Date dateEndIns) {
+		this.atletas = new ArrayList<Atleta>();
+		this.participantes = new ArrayList<Inscrito>();
+
+		this.nombre = name;
+		this.fechaCompeticion = dateComp;
+		this.fechaFinInscripcion = dateEndIns; 
+		this.precio = price;
+		this.tipo = type;
+	}
+
+	public ArrayList<Atleta> getAtletas() {
+		return this.atletas;
+	}
+
+	public ArrayList<Inscrito> getInscritosEvento() {
+		return this.participantes;
+	}
+
+	public String toString() {
 		return "Informacion del evento";
 	}
+
+	public void añadirAtleta(Atleta a) {
+		if (a != null) {
+			this.atletas.add(a);
+		}
+	}
+
+	public void aladirInscrito(Inscrito i) {
+		if (i != null) {
+			this.participantes.add(i);
+		}
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public int getPrecio() {
+		return precio;
+	}
+
+	public Date getFechaCompeticion() {
+		return fechaCompeticion;
+	}
+
+	public Date getFechaFinInscripcion() {
+		return fechaFinInscripcion;
+	}
+	
 }
