5aef8ff41cb754443299d7425698c59871af56ee
ivanpsl <uo238031@EII-L-03-12.aulas.uniovi.es>
2016-11-22 12:56:48 +0100

    Actualizado ResultadosAtletas

--- IPS/src/igu/DialogResultadosAtleta.java
+++ IPS/src/igu/DialogResultadosAtleta.java
@@ -24,6 +24,14 @@ import javax.swing.JScrollPane;
 import oracle.net.aso.e;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.BoxLayout;
+import javax.swing.JList;
+import java.awt.GridLayout;
+import javax.swing.border.TitledBorder;
+import javax.swing.UIManager;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class DialogResultadosAtleta extends JDialog {
 
@@ -33,13 +41,15 @@ public class DialogResultadosAtleta extends JDialog {
 	private static final long serialVersionUID = 1L;
 	
 	private final JPanel contentPanel = new JPanel();
-	JPanel pnContenedor;
 	Gestor g;
 	ArrayList<Inscripcion> inscripciones = new ArrayList<Inscripcion>();
 	Atleta atleta;
 	panelCuadroResultadosAtletaEvento cResultados;
 	GestorClasificaciones gC = new GestorClasificaciones();
 	VentanaPrincipal vP;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
 	/**
 	 * Create the dialog.
 	 * 
@@ -57,11 +67,77 @@ public class DialogResultadosAtleta extends JDialog {
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(new BorderLayout(0, 0));
 		{
-			JScrollPane scrollPane = new JScrollPane();
-			contentPanel.add(scrollPane);
+			JPanel pnLista = new JPanel();
+			contentPanel.add(pnLista, BorderLayout.WEST);
+			pnLista.setLayout(new BorderLayout(0, 0));
 			{
-				pnContenedor = new JPanel();
-				scrollPane.setViewportView(pnContenedor);
+				JPanel panel = new JPanel();
+				panel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Eventos inscritos:", TitledBorder.CENTER, TitledBorder.BELOW_TOP, null, new Color(0, 0, 0)));
+				pnLista.add(panel);
+				panel.setLayout(new BorderLayout(0, 0));
+				{
+					JScrollPane scrollPane = new JScrollPane();
+					panel.add(scrollPane, BorderLayout.CENTER);
+					{
+						JList list = new JList();
+						scrollPane.setViewportView(list);
+					}
+				}
+			}
+		}
+		{
+			JPanel pnInformacion = new JPanel();
+			contentPanel.add(pnInformacion, BorderLayout.CENTER);
+			pnInformacion.setLayout(new BorderLayout(0, 0));
+			{
+				JPanel pnInformacionEvento = new JPanel();
+				pnInformacionEvento.setBorder(new TitledBorder(null, "Evento:", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+				pnInformacion.add(pnInformacionEvento, BorderLayout.NORTH);
+				pnInformacionEvento.setLayout(new BorderLayout(0, 0));
+				{
+					JPanel panel = new JPanel();
+					pnInformacionEvento.add(panel);
+					{
+						JLabel label = new JLabel("New label");
+						panel.add(label);
+					}
+					{
+						textField = new JTextField();
+						panel.add(textField);
+						textField.setColumns(10);
+					}
+				}
+				{
+					JPanel panel = new JPanel();
+					pnInformacionEvento.add(panel);
+					{
+						JLabel label = new JLabel("New label");
+						panel.add(label);
+					}
+					{
+						textField_1 = new JTextField();
+						textField_1.setColumns(10);
+						panel.add(textField_1);
+					}
+				}
+				{
+					JPanel panel = new JPanel();
+					pnInformacionEvento.add(panel);
+					{
+						JLabel label = new JLabel("New label");
+						panel.add(label);
+					}
+					{
+						textField_2 = new JTextField();
+						textField_2.setColumns(10);
+						panel.add(textField_2);
+					}
+				}
+			}
+			{
+				JPanel pnInformacionResultados = new JPanel();
+				pnInformacionResultados.setBorder(new TitledBorder(null, "Resultados:", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+				pnInformacion.add(pnInformacionResultados);
 			}
 		}
 		{
@@ -69,7 +145,7 @@ public class DialogResultadosAtleta extends JDialog {
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
-				JButton cancelButton = new JButton("Cancel");
+				JButton cancelButton = new JButton("Cerrar");
 				cancelButton.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent arg0) {
 						dispose();
@@ -113,7 +189,7 @@ public class DialogResultadosAtleta extends JDialog {
 		for(Inscripcion ins: inscripciones){
 			if(obtenerCategoriaPerteneciente(ins)!=null){
 				pertenece=obtenerCategoriaPerteneciente(ins);
-				pnContenedor.add(new panelCuadroResultadosAtletaEvento(ins, g.obtenerEventoPorId(ins.getIdEvento()),gC.obtenerPosicionAbsoluta(ins, g.obtenerEventoPorId(ins.getIdEvento())) , gC.obtenerPosicion(ins,pertenece)));
+				//pnContenedor.add(new panelCuadroResultadosAtletaEvento(ins, g.obtenerEventoPorId(ins.getIdEvento()),gC.obtenerPosicionAbsoluta(ins, g.obtenerEventoPorId(ins.getIdEvento())) , gC.obtenerPosicion(ins,pertenece)));
 			}
 		}
 	}
